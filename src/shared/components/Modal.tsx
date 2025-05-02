e277e30b7d6561da120fe840143df5d466682f91
ismaelsevidanes <alum.isevidanesd@iesalixar.org>
2025-05-02 21:09:29 +0200

    Modal login y register mejorado

--- src/shared/components/Modal.tsx
+++ src/shared/components/Modal.tsx
@@ -30,7 +30,6 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   return ReactDOM.createPortal(
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="close-button" onClick={onClose}>X</button>
         {children}
       </div>
     </div>,
